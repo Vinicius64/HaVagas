d18264b8fddc3e571f775191a630f040661956d2
Vinicius64 <oliveira.vinicius64@gmail.com>
2024-08-25 19:47:22 -0300

    feat(MainActivity): integrado ActivityMainBinding

--- app/src/main/java/com/vin/pdms5/havagas/MainActivity.kt
+++ app/src/main/java/com/vin/pdms5/havagas/MainActivity.kt
@@ -2,7 +2,12 @@ package com.vin.pdms5.havagas
 
 
 import androidx.appcompat.app.AppCompatActivity
+import com.vin.pdms5.havagas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
 
 }
